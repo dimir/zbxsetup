892dad8c29f359f126400b5ff7db6603cdb66c88
dimir <vladimir.levijev@zabbix.com>
2026-02-26 13:53:12 +0000

    combined contents of history tables into one output, added 3 digits precision to double values

--- rsm-dump-history.sh
+++ rsm-dump-history.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env bash
 
-declare select_fields="h.host,i.key_,s.itemid,concat(from_unixtime(s.clock), ' (', s.clock, ')') as clock,s.value"
-declare order="order by h.host,i.key_,s.clock"
+declare select_fields="h.host,i.key_,s.itemid,concat(from_unixtime(s.clock), ' (', s.clock, ')') as clock,cast(s.value as double) as value"
+declare order="order by host,key_,clock"
 declare clock_field="s.clock"
 declare -a search_columns=(h.host i.key_)
 
 (
 	source .rsm-dump
 
-	declare cond
+	declare sql=""
 	for i in history_uint history; do
-		echo "  $i:"
-		cond="${ptrn_cond}"
-		[[ $i =~ lastvalue ]] || cond+="${time_cond}"
-		db-exec.sh "select ${select_fields}
+		[ -n "$sql" ] && sql+=" union "
+
+		declare fields="${select_fields}"
+
+		[ $i = "history" ] && fields="${fields/cast(s.value as double)/round(s.value, 3)}"
+
+		echo "$i: $fields"
+
+		sql+="select ${fields}
 				from ${i} s,items i,hosts h,hstgrp g,hosts_groups hg
 				where hg.hostid=h.hostid
 					and hg.groupid=g.groupid
 					and h.hostid=i.hostid
 					and i.itemid=s.itemid
-					and g.groupid in ($TLDS,$PMON,$PRES) ${cond}${order}" -t
+					and g.groupid in ($TLDS,$PMON,$PRES) ${ptrn_cond}${time_cond}"
 	done
+
+	db-exec.sh "${sql}${order}" -t
 ) 2>&1 | grep -v '^\['
